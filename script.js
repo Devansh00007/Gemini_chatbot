39fb4badd175c6903ee5a13d46ab7d05a3f5aaaa
Devansh00007 <147805436+Devansh00007@users.noreply.github.com>
2025-11-07 02:17:58 +0530

    Change API key in script.js
    Updated API key for the API_URL.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const deleteChatButton = document.querySelector("#delete-chat-button");
 let userMessage = null;
 let isResponseGenerating = false;
 
-const API_KEY = "AIzaSyALV8r7Z51MRIywjoraQ1-Kgrc3yWCs3wg";
+const API_KEY = "AIzaSyDYK0RC7J7NybXsiqCukcFfyVGhv1qU-sw";
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 const loadDataFromLocalstorage = () => {
